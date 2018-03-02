ed6d00e61378675ec5f28edc7b91c90227087ceb
Kaila Goff <kaila.goff@gmail.com>
2018-03-02 15:11:51 -0800

    integration spec passing, updating store name

--- spec/brand_store_integration_spec.rb
+++ spec/brand_store_integration_spec.rb
@@ -23,3 +23,18 @@ describe 'the store creation path', {:type => :feature} do
     expect(page).to have_content('Adidas Outlet')
   end
 end
+
+describe 'the store creation path', {:type => :feature} do
+  it 'takes the user to a page where they can see stores and edit them' do
+    visit '/'
+    click_link('Add a New Show Store Location')
+    fill_in('Store Name', :with => 'adidas outlet')
+    click_button('Add Store')
+    expect(page).to have_content('Adidas Outlet')
+    click_link('Adidas Outlet')
+    click_link('Edit Store name')
+    fill_in('Edit your store:', :with => 'nike outlet')
+    click_button('Update')
+    expect(page).to have_content('Home Stores Brands Keep Track of Your Stores and Brands Add a New Show Store Location Add a New Shoe Brand')
+  end
+end
