873b2e1ad10d37b1cb85717143f443d0720fd55f
Kaila Goff <kaila.goff@gmail.com>
2018-03-02 08:34:01 -0800

    set-up database and bundle Gemfile

---
+++ db/migrate/20180302162820_create_brands.rb
@@ -0,0 +1,10 @@
+class CreateBrands < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:brands) do |t|
+      t.column(:name, :string)
+      t.column(:price, :int)
+
+      t.timestamps()
+    end
+  end
+end
---
+++ db/migrate/20180302163213_create_brands_stores.rb
@@ -0,0 +1,10 @@
+class CreateBrandsStores < ActiveRecord::Migration[5.1]
+  def change
+    create_table(:brands_stores) do |t|
+      t.column(:brand_id, :int)
+      t.column(:store_id, :int)
+
+      t.timestamps()
+    end
+  end
+end
