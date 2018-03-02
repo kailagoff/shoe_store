4740ef6cfbbdde7367ee23e5442b55f43dd0021a
Kaila Goff <kaila.goff@gmail.com>
2018-03-02 15:05:23 -0800

    second integration passing, describing the path of the store creation

--- spec/brand_store_integration_spec.rb
+++ spec/brand_store_integration_spec.rb
@@ -7,9 +7,19 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe 'the store creation path', {:type => :feature} do
-  it 'the user starts on the home page where they can create a store' do
+  it 'the user starts on the home page' do
     visit '/'
     click_link('Add a New Show Store Location')
     expect(page).to have_content()
+  end
+end
+
+describe 'the store creation path', {:type => :feature} do
+  it 'takes the user to a form where they can create a store' do
+    visit '/'
+    click_link('Add a New Show Store Location')
+    fill_in('Store Name', :with => 'adidas outlet')
+    click_button('Add Store')
+    expect(page).to have_content('Adidas Outlet')
   end
 end
