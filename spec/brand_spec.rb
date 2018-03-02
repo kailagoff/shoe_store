4123d870681da14713fd2cc1122ec9f1e3dd2aa2
Kaila Goff <kaila.goff@gmail.com>
2018-03-02 08:39:22 -0800

    first meaningful fail for validation brand spec

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,9 @@
 require 'spec_helper'
+
+
+describe(Brand) do
+  it("will validate the presence of a name") do
+    brand = Brand.new({:name => ""})
+    expect(brand.save()).to(eq(false))
+  end
+end
